4368a85f5b7ad8b85d13187cfe5cff5af4e663bb
YaakovG <ygoldenbergyo@gmail.com>
2024-04-25 23:58:08 -0400

    yeah

--- ORM-Dapper/Program.cs
+++ ORM-Dapper/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace ORM_Dapper
-{
-    public class Program
+﻿using System;
+using System.Data;
+using System.IO;
+using MySql.Data.MySqlClient;
+using Microsoft.Extensions.Configuration;
+using ORM_Dapper;
+using System.Transactions;
+//^^^^MUST HAVE USING DIRECTIVES^^^^
+namespace ORM_Dapper {
+    
+    public class Program {
+
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        var config = new ConfigurationBuilder()
+                        .SetBasePath(Directory.GetCurrentDirectory())
+                        .AddJsonFile("appsettings.json")
+                        .Build();
+        string connString = config.GetConnectionString("DefaultConnection");
+        IDbConnection conn = new MySqlConnection(connString);
+        var repo = new DapperDepartmentRepository(conn);
+
+        Console.WriteLine("Type a new Department name");
+        var newDepartment = Console.ReadLine();
+        repo.InsertDepartment(newDepartment);
+        var departments = repo.GetAllDepartments();
+        foreach (var department in departments)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(department.Name);
         }
     }
 }
+    }
